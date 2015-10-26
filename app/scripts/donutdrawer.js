e70c139341ec20ad895968d112a92379eea47c33
Eliot Fallon <Eliot@Eliots-Air.home>
2015-10-26 08:20:48 +0000

    Half updated canvas draw-er

--- app/scripts/donutdrawer.js
+++ app/scripts/donutdrawer.js
@@ -1,40 +1,63 @@
     /* Since this is just one graph there is no requirement to load whole library.
-    *  This function allows us to draw a graph more limited in how it can appear.
-    *  If more flexibilty is required when the graph is used as part of a larger application the library may become more useful.
-    */
+     *  This function allows us to draw a graph more limited in how it can appear.
+     *  If more flexibilty is required when the graph is used as part of a larger application the library may become more useful.
+     */
+
+    var requestAnimationFrame =
+        window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        window.msRequestAnimationFrame ||
+        window.oRequestAnimationFrame ||
+        function(callback) {
+            return setTimeout(callback, 1);
+        };
 
     function donutDrawer(canvasId, dataSet) {
 
-            var X = 180;
-            var Y = 180;
-            var outterRadius = 150;
-            var innerRadius = 110;
- 
-            var c = document.getElementById(canvasId);
-             
+        //These are fixed for this graph, but could be varied if required if the context of the graph use required it.
+        var c = document.getElementById(canvasId);
+        var X = canvas.width / 2;
+        var Y = canvas.height / 2;
+        var medianRadius = 150;
+        var duration = 2;
+        //
 
-            var context = c.getContext("2d");
-             
+        var context = c.getContext("2d");
 
-            drawDonut(Math.PI * 0.32, Math.PI * 0.5);
+        var start = new Date().getTime();
+        var end = start + duration;
 
-            drawDonut(Math.PI*0.5 , Math.PI* 1.5);
+        function callRender() {
+            context.clearRect(0, 0, canvas.width, canvas.height);
 
-            drawDonut(Math.PI * 1.5, Math.PI*2);
-             
-            drawDonut(0, Math.PI*.32);
-             
-            //*******************************************************//\        
-            // drawDonut() function drawes 2 full or partial circles inside each other one clockwise and the other is counter-clockwise
-            function drawDonut(sRadian, eRadian){
-                 
-                context.beginPath();
-                    context.arc(X, Y, medianRadius, sRadian, eRadian, false); // Outer: CCW
-                    canvas.lineWidth = this.lineWidth;
-                    canvas.strokeStyle = this.strockStyle;
-                    ctx.stroke();
-                context.closePath();
+            //How far into the animation are we?
+            var timestamp = new Date().getTime();
+            var progress = Math.min((duration - (end - timestamp)) / duration, 1);
+
+            for(var i=0; i<dataSet.length; i++){
+                //Need to draw for each dataset
+                drawDonut(Math.PI * 0.32, Math.PI * 0.5);    
             }
+            
+
+
+            // Redraw
+            requestAnimationFrame(callRender);
+        }
+
+        //*******************************************************//\        
+        // drawDonut() function drawes 2 full or partial circles inside each other one clockwise and the other is counter-clockwise
+        function drawDonut(sRadian, eRadian, color) {
+            context.beginPath();
+            context.arc(X, Y, medianRadius, sRadian, eRadian, false); // Outer: CCW
+            canvas.lineWidth = 143;
+            canvas.strokeStyle = color;
+            ctx.stroke();
+            context.closePath();
+
+            // Redraw
+
+        }
 
     }
-            
